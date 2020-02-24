a605be9f7d8dba64acc2a9acef0ae24b7101d126
jfields <jthediamondminer@gmail.com>
2020-02-23 23:01:48 -0600

    Initial commit

---
+++ app/src/main/java/com/jfields/criticalthinking/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.jfields.criticalthinking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.gesture.GestureLibraries
+import android.gesture.GestureLibrary
+import android.gesture.GestureOverlayView
+import android.gesture.GestureOverlayView.OnGesturePerformedListener
+import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import android.gesture.Gesture
+import android.view.View
+
+class MainActivity : AppCompatActivity(), OnGesturePerformedListener {
+
+    var gLibrary: GestureLibrary? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        gestureSetup()
+
+
+    }
+
+
+    private fun gestureSetup() {
+        gLibrary = GestureLibraries.fromRawResource(this, R.raw.gestures)
+        if (gLibrary?.load() == false) {
+            finish()
+        }
+
+        gOverlay.addOnGesturePerformedListener(this)
+    }
+
+    override fun onGesturePerformed(overlay: GestureOverlayView, gesture: Gesture) {
+
+        val predictions = gLibrary?.recognize(gesture)
+
+        predictions?.let {
+            if (it.size > 0 && it[0].score > 1.0) {
+                val action = it[0].name
+                statusText.text = action
+            }
+        }
+    }
+}
